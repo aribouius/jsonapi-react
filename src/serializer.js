0396cf1228bd923bac814dcbf66e0a25fca44ab1
Ari Bouius <ari@roaminghunger.com>
2020-03-04 12:20:58 -0800

    Fix typo

--- src/serializer.js
+++ src/serializer.js
@@ -173,7 +173,7 @@ export class Serializer {
     if (Array.isArray(res.data)) {
       data = data.reduce(
         (acc, rec) =>
-          res.data.find(r => r.id === rec.id && r.type === res.type)
+          res.data.find(r => r.id === rec.id && r.type === rec.type)
             ? acc.concat(rec.attributes)
             : acc,
         []
