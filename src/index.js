f1cb1d75f41354e575ddf86d0b76ac378b8f4c50
Ari Bouius <ari@roaminghunger.com>
2020-06-06 16:26:47 -0700

    Export dispatch actions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ export { ApiClient } from './client'
 export { useQuery, useMutation, useClient, useIsFetching } from './hooks'
 export { renderWithData } from './render'
 export { Serializer } from './serializer'
+export {
+  REQUEST_QUERY,
+  RECEIVE_QUERY,
+  REQUEST_MUTATION,
+  RECEIVE_MUTATION
+} from './constants'
